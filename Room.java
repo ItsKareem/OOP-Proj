ea564605d5dd100d513b4f0f8dd757929d563de1
ItsKareem <1022027.kareem@stemegypt.edu.eg>
2026-04-10 17:40:15 +0200

    Added Room and DisplayRoomDetails

--- Room.java
+++ Room.java
@@ -38,8 +38,38 @@ public class Room {
         isAvailable = available;
     }
 
-    public int getRoomNumber() { return roomNumber; }
-    public RoomType getRoomType() { return roomType; }
-    public List<Amenity> getAmenities() { return amenities; }
-    public boolean isAvailable() { return isAvailable; }
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public List<Amenity> getAmenities() {
+        return amenities;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("----- Room Details -----");
+        System.out.println("Room Number: " + roomNumber);
+        System.out.println("Room Type: " + roomType.getName());
+        System.out.println("Price per Night: " + roomType.getPricePerNight());
+        System.out.println("Available: " + (isAvailable ? "Yes" : "No"));
+
+        System.out.println("Amenities:");
+        if (amenities.isEmpty()) {
+            System.out.println("  None");
+        } else {
+            for (Amenity amenity : amenities) {
+                System.out.println("  - " + amenity.getName());
+            }
+        }
+
+        System.out.println("------------------------");
+    }
 }
